71b72060232fb49c93a022c0c101b33fb0bff506
Unknown <Unknown@unknown.unknown>
2024-10-29 20:41:03 +0300

    Initial Commit

---
+++ SwiftUISampleDemoApp/SwiftUISampleDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleDemoAppApp.swift
+//  SwiftUISampleDemoApp
+//
+//  Created by Admin on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
